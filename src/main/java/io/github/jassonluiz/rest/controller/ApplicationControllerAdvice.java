f2f5f8e638855dd022f1dee9da25fed877c34395
JassonLuiz <jassonluizjr@hotmail.com>
2022-07-20 19:50:18 -0300

    Adiciona o DevTools e Cria o controlador de Erros

---
+++ src/main/java/io/github/jassonluiz/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,20 @@
+package io.github.jassonluiz.rest.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import io.github.jassonluiz.exception.RegraNegocioException;
+import io.github.jassonluiz.rest.ApiErrors;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+	
+	@ExceptionHandler(RegraNegocioException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
+		String mensagemErro = ex.getMessage();
+		return new ApiErrors(mensagemErro);
+	}
+}

11c51695ddaa245aed4660e3b208b91dbbfbfcef
JassonLuiz <jassonluizjr@hotmail.com>
2022-09-05 18:43:04 -0300

    Adiciona o Start Validation e testa o Bean Validation

--- src/main/java/io/github/jassonluiz/domain/entity/Cliente.java
+++ src/main/java/io/github/jassonluiz/domain/entity/Cliente.java
@@ -10,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -29,6 +30,7 @@ public class Cliente {
 	private Integer id;
 	
 	@Column(length = 100)
+	@NotEmpty(message = "Campo nome é obrigatório!")
 	private String nome;
 	
 	@Column(name = "cpf", length = 11)
--- src/main/java/io/github/jassonluiz/rest/controller/ClienteController.java
+++ src/main/java/io/github/jassonluiz/rest/controller/ClienteController.java
@@ -2,6 +2,8 @@ package io.github.jassonluiz.rest.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
@@ -38,7 +40,7 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente salvar( @RequestBody Cliente cliente ) {
+	public Cliente salvar( @RequestBody @Valid Cliente cliente ) {
 		return repository.save(cliente);
 	}
 	

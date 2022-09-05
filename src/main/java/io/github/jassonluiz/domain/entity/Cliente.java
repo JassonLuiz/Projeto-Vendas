a4a2f415e35a1f363ba346e07a3675fc748afb68
JassonLuiz <jassonluizjr@hotmail.com>
2022-09-05 18:50:06 -0300

    Valida entidade Cliente

--- src/main/java/io/github/jassonluiz/domain/entity/Cliente.java
+++ src/main/java/io/github/jassonluiz/domain/entity/Cliente.java
@@ -12,6 +12,8 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
@@ -34,6 +36,8 @@ public class Cliente {
 	private String nome;
 	
 	@Column(name = "cpf", length = 11)
+	@NotEmpty(message = "Campo CPF é obrigatótio")
+	@CPF(message = "Informe um CPF válido!")
 	private String cpf;
 	
 	@JsonIgnore
